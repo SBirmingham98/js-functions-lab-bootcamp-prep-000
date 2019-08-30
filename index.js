4f5843496642fadb8f003de05d7fa45a3d532e1b
Sean Birmingham <seannbirmingham98@gmail.com>
2019-08-30 17:10:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
-fucntion happyHolidaysTo(holiday, name) {
+function happyHolidaysTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
+
